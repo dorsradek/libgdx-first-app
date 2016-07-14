e2c748ccbd2fcecff26e6b4f9f3e0d4e0457fb37
rdors <dors.radek@gmail.com>
2016-07-14 15:01:28 +0200

    clear app

--- core/src/pl/dors/radek/MyFirstApp.java
+++ core/src/pl/dors/radek/MyFirstApp.java
@@ -3,55 +3,20 @@ package pl.dors.radek;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.utils.Logger;
 
 public class MyFirstApp extends ApplicationAdapter {
-    private Sprite sprite;
-    private Texture img;
-    private int x;
-    private int y;
 
     @Override
     public void create() {
-        x = getScreenCenterX();
-        y = getScreenCenterY();
-        sprite = new Sprite(img);
-        logger.error("Image size: [" + img.getWidth() + ", " + img.getHeight() + "]");
-        logger.error("Image position: [" + x + ", " + y + "]");
-    }
-
-    @Override
-    public void resize(int width, int height) {
     }
 
     @Override
     public void render() {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
     }
 
     @Override
     public void dispose() {
-    }
-
-    private void drawOnScreen() {
-        sprite.setPosition(
-                sprite.getX() + 2.5f * Gdx.graphics.getDeltaTime(),
-                sprite.getY() + 2.5f * Gdx.graphics.getDeltaTime());
-        sprite.draw(batch);
-    }
-
-    private int getScreenCenterX() {
-        int width = Gdx.app.getGraphics().getWidth();
-        int imageWidth = img.getWidth();
-        return (width / 2) - (imageWidth / 2);
-    }
-
-    private int getScreenCenterY() {
-        int height = Gdx.app.getGraphics().getHeight();
-        int imageHeight = img.getHeight();
-        return (height / 2) - (imageHeight / 2);
     }
 }
