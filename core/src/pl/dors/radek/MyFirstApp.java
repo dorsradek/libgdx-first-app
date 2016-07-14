080ef813d68caa6e69a6d0fb3bc83d675db9a522
rdors <dors.radek@gmail.com>
2016-07-14 14:34:58 +0200

    custom app

--- core/src/pl/dors/radek/MyFirstApp.java
+++ core/src/pl/dors/radek/MyFirstApp.java
@@ -4,30 +4,65 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Logger;
 
 public class MyFirstApp extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    Logger logger = new Logger("FirstGame");
+    private SpriteBatch batch;
+    private Sprite sprite;
+    private Texture img;
+    private int x;
+    private int y;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture(Gdx.files.internal("badlogic.jpg"));
+        x = getScreenCenterX();
+        y = getScreenCenterY();
+        sprite = new Sprite(img);
+        logger.error("Image size: [" + img.getWidth() + ", " + img.getHeight() + "]");
+        logger.error("Image position: [" + x + ", " + y + "]");
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        drawOnScreen();
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
+
+    private void drawOnScreen() {
+        sprite.setPosition(
+                sprite.getX() + 2.5f * Gdx.graphics.getDeltaTime(),
+                sprite.getY() + 2.5f * Gdx.graphics.getDeltaTime());
+        sprite.draw(batch);
+    }
+
+    private int getScreenCenterX() {
+        int width = Gdx.app.getGraphics().getWidth();
+        int imageWidth = img.getWidth();
+        return (width / 2) - (imageWidth / 2);
+    }
+
+    private int getScreenCenterY() {
+        int height = Gdx.app.getGraphics().getHeight();
+        int imageHeight = img.getHeight();
+        return (height / 2) - (imageHeight / 2);
+    }
 }
