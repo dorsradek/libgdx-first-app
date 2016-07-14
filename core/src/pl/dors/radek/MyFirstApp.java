5741cf1dbb381f22f533fa3b3652e06ca125a697
rdors <dors.radek@gmail.com>
2016-07-14 14:58:25 +0200

    clear app

--- core/src/pl/dors/radek/MyFirstApp.java
+++ core/src/pl/dors/radek/MyFirstApp.java
@@ -3,14 +3,10 @@ package pl.dors.radek;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Logger;
 
 public class MyFirstApp extends ApplicationAdapter {
-    Logger logger = new Logger("FirstGame");
-    private SpriteBatch batch;
     private Sprite sprite;
     private Texture img;
     private int x;
@@ -18,8 +14,6 @@ public class MyFirstApp extends ApplicationAdapter {
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        img = new Texture(Gdx.files.internal("badlogic.jpg"));
         x = getScreenCenterX();
         y = getScreenCenterY();
         sprite = new Sprite(img);
@@ -33,18 +27,13 @@ public class MyFirstApp extends ApplicationAdapter {
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        batch.begin();
-        drawOnScreen();
-        batch.end();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        img.dispose();
     }
 
     private void drawOnScreen() {
